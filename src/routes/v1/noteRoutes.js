770a809d998e5ebe2f1a50ad64443e4e6f28c82f
Priyanka Negi <prinegi100@gmail.com>
2017-12-08 23:38:32 +0530

    -fix eslint

--- src/routes/v1/noteRoutes.js
+++ src/routes/v1/noteRoutes.js
@@ -2,79 +2,78 @@ const noteRoutes = require('express').Router();
 const Note     = require('../../models/Note');
 
 noteRoutes.route('/')
-  .get((req, res) => {
+	.get((req, res) => {
+		// finding whole data
+		Note.find({})
+			.then((data) => {
+				// console.log('data from db: ');
+				return res.status(200).json({
+					data: data,
+					status: 'ok',
+					message: 'get data successfully'
+				});
+			})
+			.catch((err) => {
+				return res.status(404).json({
+					data:[],
+					status: 'failed',
+					message: 'data not found'
+				});
+			});
+	})
 
-    // finding whole data
-    Note.find({})
-      .then((data) => {
-        // console.log('data from db: ');
-        return res.status(200).json({
-          data: data,
-          status: 'ok',
-          message: 'get data successfully'
-        });
-      })
-      .catch((err) => {
-        return res.status(404).json({
-          data:[],
-          status: 'failed',
-          message: 'data not found'
-        });
-      });
-    })
-
-  .post((req, res) => {
-    // console.log('request from body',req.body);
-    const {note} = req.body;
-       if(!note || !done){
-      return res.status(400).json({
-        data: [],
-        status:'parameters are missing'
-      });
-    }
-    const oneNotes = {
-      note
-    };
+	.post((req, res) => {
+		// console.log('request from body',req.body);
+		const {note} = req.body;
+		if(!note || !done){
+			return res.status(400).json({
+				data: [],
+				status:'parameters are missing'
+			});
+		}
+		const oneNotes = {
+			note
+		};
     
-    let myNotes = new Note(oneNotes);
+		let myNotes = new Note(oneNotes);
  
-    console.log(myNotes);
+		console.log(myNotes);
 
-    //saving data into database    
-    myNotes.save()
-    .then((data) => {
-      // console.log('*****data from db', data);   //note data is not saving
-      return res.status(200).json({
-        data:data,
-        status:'ok',
-      });
-    })
-    .catch((err) => {
-      return res.status(404).json({
-        data:[],
-        status:'failed'
-      });
-    });
+		//saving data into database    
+		myNotes.save()
+			.then((data) => {
+				// console.log('*****data from db', data);   //note data is not saving
+				return res.status(200).json({
+					data:data,
+					status:'ok',
+				});
+			})
+			.catch((err) => {
+				return res.status(404).json({
+					data:[],
+					status:'failed'
+				});
+			});
 
-  });
+	});
 
-  //delete route
-  noteRoutes.route('/:id')
-    .delete((req, res) => {
-      Note.findByIdAndRemove({_id: req.params.id})
-      .then((data) =>{
-        console.log('deleted')
-            res.send(data);
-        })
-      .catch(() => {
-        return res.status(404).json({
-          data: [],
-          status: 'something went wrong'
-        });
-      })
+//delete route
+noteRoutes.route('/:id')
+	.delete((req, res) => {
+		Note.findByIdAndRemove({_id: req.params.id})
+			.then((data) =>{
+				console.log('deleted');
+				res.send(data);
+			})
+			.catch(() => {
+				return res.status(404).json({
+					data: [],
+					status: 'something went wrong'
+				});
+			});
       
-    })
+	});
 
 
 
-  module.exports = noteRoutes;  
+module.exports = noteRoutes;  
